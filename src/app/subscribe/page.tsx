07236b5d00df8c75692f54f66a18d01b8a8d1d31
Gwen Lim <gwenlim1985@gmail.com>
2026-03-22 03:27:18 +0000

    What do you think of this idea in general?

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const TIERS = [
   {
